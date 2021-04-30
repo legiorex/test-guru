f36638ff8084b98a6a10b69d28438e0a19f9d71a
legiorex <batyushev@gmail.com>
2021-04-30 20:07:50 +0300

    fix model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
-  has_many :tests, foreign_key: :author_id
+  has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
 
   def used_tests(level)
     Test.joins(:tests_users).where('tests_users.user_id' => id,
