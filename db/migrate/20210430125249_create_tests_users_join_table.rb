32b83187b6f82e778d5c624a07cdda5db20bbbcd
legiorex <batyushev@gmail.com>
2021-05-02 21:32:55 +0300

    remove comment

--- db/migrate/20210430125249_create_tests_users_join_table.rb
+++ db/migrate/20210430125249_create_tests_users_join_table.rb
@@ -2,7 +2,6 @@ class CreateTestsUsersJoinTable < ActiveRecord::Migration[6.1]
   def change
     create_join_table :users, :tests do |t|
       t.index %i[user_id test_id]
-      # t.index [:test_id, :user_id]
     end
   end
 end
