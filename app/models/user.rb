3ffb7214119dc881f3533182cc0c303cdfa0b2e7
legiorex <batyushev@gmail.com>
2021-05-26 20:05:51 +0300

    lesson-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
 
-  validates :email, presence: true
+  has_secure_password
+
+  validates :email, uniqueness: true,
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'invalid format email', on: :create }
 
   def used_tests(level)
     tests.where(level: level)
