88a2bc177648b09bb3e67b1812099146200629b4
legiorex <batyushev@gmail.com>
2021-06-10 12:22:41 +0300

    fix comment reviewer

--- app/javascript/utillites/check_psw.js
+++ app/javascript/utillites/check_psw.js
@@ -16,17 +16,18 @@ const onChangeConfirmPassword = (e, passwordInput) => {
     if(!e.target.value){
         e.target.classList.remove('is-valid')
         e.target.classList.remove('is-invalid')
-    } else {
-
-        if(passwordInput.value === e.target.value) {
-            e.target.classList.add('is-valid')
-            e.target.classList.remove('is-invalid')
-        } else {
-            e.target.classList.add('is-invalid')
-            e.target.classList.remove('is-valid')
-        }
-
     }
+
+    if(passwordInput.value === e.target.value && e.target.value) {
+        e.target.classList.add('is-valid')
+        e.target.classList.remove('is-invalid')
+    }
+    if (passwordInput.value !== e.target.value && e.target.value) {
+        e.target.classList.add('is-invalid')
+        e.target.classList.remove('is-valid')
+    }
+
+
 
 
 }
@@ -35,16 +36,15 @@ const onChangePassword = (e, confirmPasswordInput) => {
     if(!confirmPasswordInput.value){
         confirmPasswordInput.classList.remove('is-valid')
         confirmPasswordInput.classList.remove('is-invalid')
-    } else {
-
-        if(confirmPasswordInput.value === e.target.value) {
-            confirmPasswordInput.classList.add('is-valid')
-            confirmPasswordInput.classList.remove('is-invalid')
-        } else {
-            confirmPasswordInput.classList.add('is-invalid')
-            confirmPasswordInput.classList.remove('is-valid')
-        }
     }
 
+    if(confirmPasswordInput.value === e.target.value && confirmPasswordInput.value) {
+        confirmPasswordInput.classList.add('is-valid')
+        confirmPasswordInput.classList.remove('is-invalid')
+    }
 
+    if (confirmPasswordInput.value !== e.target.value && confirmPasswordInput.value) {
+        confirmPasswordInput.classList.add('is-invalid')
+        confirmPasswordInput.classList.remove('is-valid')
+    }
 }
