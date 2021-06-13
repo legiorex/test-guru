6279447fec63c6e61fd9ac2838caaea43b675bc5
legiorex <batyushev@gmail.com>
2021-06-13 20:45:44 +0300

    remove test seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,13 +18,7 @@ get_users = users.map { |item| User.find_or_create_by(item) }
 
 tests = [
   { title: 'types in Ruby', category_id: get_categories[0].id, author_id: get_users[0].id },
-  { title: 'types in JavaScript', category_id: get_categories[1].id, author_id: get_users[1].id },
-  { title: 'Классы в Ruby', level: 1, category_id: get_categories[0].id, author_id: get_users[0].id },
-  { title: 'Блоки', level: 2, category_id: get_categories[0].id, author_id: get_users[0].id },
-  { title: 'Массивы в Ruby', level: 0, category_id: get_categories[0].id, author_id: get_users[1].id },
-  { title: 'Стрелочные функции', level: 1, category_id: get_categories[1].id, author_id: get_users[1].id },
-  { title: 'Функции генераторы', level: 3, category_id: get_categories[1].id, author_id: get_users[1].id },
-  { title: 'Асинхронные операции', level: 3, category_id: get_categories[1].id, author_id: get_users[1].id }
+  { title: 'types in JavaScript', category_id: get_categories[1].id, author_id: get_users[1].id }
 ]
 get_tests = tests.map { |item| Test.find_or_create_by(item) }
 
@@ -66,11 +60,6 @@ answers.each { |item| Answer.find_or_create_by(item) }
 
 tests_users = [
   { test_id: get_tests[0].id, user_id: get_users[0].id },
-  { test_id: get_tests[1].id, user_id: get_users[0].id },
-  { test_id: get_tests[7].id, user_id: get_users[0].id },
-  { test_id: get_tests[6].id, user_id: get_users[0].id },
-  { test_id: get_tests[3].id, user_id: get_users[0].id },
-  { test_id: get_tests[1].id, user_id: get_users[1].id }
-
+  { test_id: get_tests[1].id, user_id: get_users[0].id }
 ]
 tests_users.map { |item| TestsUser.find_or_create_by(item) }
