13c9adc1163dd3333157517270c66f4a83e551cc
legiorex <batyushev@gmail.com>
2021-07-08 20:45:24 +0300

    fix

--- app/services/badge_award_service.rb
+++ app/services/badge_award_service.rb
@@ -4,16 +4,16 @@ class BadgeAwardService
   end
 
   def call
-    if first_passage_test && (@current_test_passage.result_percent == 100)
+    if first_passage_test && @current_test_passage.success
       badge = UserBadge.new
       badge.user_id = @current_test_passage.user_id
-      badge.badge_id = find_badge(2).id
+      badge.badge_id = find_badge('start').id
       badge.save
     end
   end
 
   def first_passage_test
-   TestPassage.where(test_id: @current_test_passage.test_id).size == 1
+    TestPassage.where(test_id: @current_test_passage.test_id).size == 1
   end
 
   private
