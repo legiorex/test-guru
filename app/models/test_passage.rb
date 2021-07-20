5c31ce87f134c1db306a7f643288f3d41b5c17cc
legiorex <37956597+legiorex@users.noreply.github.com>
2021-07-20 15:33:20 +0300

    Revert "create table"

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -6,7 +6,6 @@ class TestPassage < ApplicationRecord
   before_validation :before_validation_set_first_question, on: :create
   before_update :before_update_next_question
 
-
   SUCCESS = 85
   SUCCESS.freeze
 
@@ -16,7 +15,6 @@ class TestPassage < ApplicationRecord
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-
     save!
   end
 
